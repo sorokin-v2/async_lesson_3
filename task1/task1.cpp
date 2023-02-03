73830a8a4690f7e7851a0d7dc1e3ba2e9bf45bf4
Valery Sorokin <podrabotka-2019@yandex.ru>
2023-02-03 11:11:36 +0300

    first commit

---
+++ task1/task1.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <future>
+
+/*
+//Синхронная реализация
+std::vector<int>::iterator get_min_index(std::vector<int>::iterator begin, std::vector<int>::iterator end)
+{
+    return  std::min_element(begin, end);
+}
+*/
+
+//Асинхронная реализация
+void get_min_index(std::vector<int>::iterator begin, std::vector<int>::iterator end, std::promise<std::vector<int>::iterator> Prom)
+{
+    Prom.set_value(std::min_element(begin, end));
+}
+
+int main()
+{
+    const size_t vector_size = 20;
+    std::vector<int> Vect(vector_size);
+    std::generate(Vect.begin(), Vect.end(), []() { return rand() % 100; });
+    std::cout << "Source:\n";
+    std::for_each(Vect.begin(), Vect.end(), [](const int val) {std::cout << val << " "; });
+    std::cout << "\nSotred:" << std::endl;
+
+    for (size_t i = 1; i < vector_size; ++i)
+    {
+        //auto min_iterator = get_min_index(Vect.begin() + i, Vect.end()); //Синхронный вызов
+        std::promise<std::vector<int>::iterator> Prom;
+        std::future<std::vector<int>::iterator> Fut = Prom.get_future();
+        std::future<void> res = std::async(get_min_index, Vect.begin() + i, Vect.end(), std::move(Prom));
+        auto min_iterator = Fut.get();
+        if (*(min_iterator) < Vect.at(i - 1))
+        {
+            std::swap(*min_iterator, Vect.at(i - 1));
+        }
+    }
+    std::for_each(Vect.begin(), Vect.end(), [](const int val) {std::cout << val << " "; });
+    std::cout << std::endl;
+    return 0;
+}
---
+++ task_2/task_2.cpp
@@ -0,0 +1,43 @@
+﻿#include <chrono>
+#include <future>
+#include <iostream>
+#include <numeric>
+#include <vector>
+
+
+template<typename Iterator>
+void parallel_for_each(Iterator first, Iterator last, std::function<void(int)> func)
+{
+	unsigned long const length = std::distance(first, last);
+	unsigned long const max_chunk_size = 25;
+	if (length <= max_chunk_size)
+	{
+		std::for_each(first, last, func);
+	}
+	else
+	{
+		Iterator mid_point = first;
+		std::advance(mid_point, length / 2);
+		std::future<void> first_half_result = std::async(std::launch::async, parallel_for_each<Iterator>, first, mid_point, func);
+		std::future<void> second_half_result = std::async(std::launch::async, parallel_for_each<Iterator>, mid_point, last, func);
+	}
+}
+
+int main()
+{
+	std::vector<int> V1(1000);
+	std::generate(V1.begin(), V1.end(), []() {return rand() % 10; });
+	auto start1 = std::chrono::high_resolution_clock::now();
+	std::for_each(V1.begin(), V1.end(), [](int val) {++val; });
+	auto stop1 = std::chrono::high_resolution_clock::now();
+	std::chrono::duration<double, std::milli> res = stop1 - start1;
+	std::cout << res.count() << std::endl;
+
+	auto start2 = std::chrono::high_resolution_clock::now();
+	parallel_for_each(V1.begin(), V1.end(), [](int val) {++val; });
+	auto stop2 = std::chrono::high_resolution_clock::now();
+	std::chrono::duration<double, std::milli> res2 = stop2 - start2;
+	std::cout << res2.count() << std::endl;
+
+	return 0;
+}
